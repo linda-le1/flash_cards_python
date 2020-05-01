957e4db2e6bda30e04925a0cb0697035aef42b08
Linda Le <yueling1222@gmail.com>
2020-04-30 21:42:56 -0600

    Add updated Turn class and tests

--- lib/turn.py
+++ lib/turn.py
@@ -2,3 +2,15 @@ class Turn:
     def __init__(self, guess, card):
         self.guess = guess
         self.card = card
+
+    def correct(self):
+        if self.guess == self.card.answer:
+            return True
+        else:
+            return False
+
+    def feedback(self):
+        if self.correct():
+            return "Great job! Correct!"
+        else:
+            return "Not quite. Try again!"
--- tests/test_turn.py
+++ tests/test_turn.py
@@ -1,6 +1,5 @@
 import unittest
 from lib.card import Card
-from lib.deck import Deck
 from lib.turn import Turn
 
 class TestingTurn(unittest.TestCase):
@@ -14,8 +13,48 @@ class TestingTurn(unittest.TestCase):
     def test_it_has_attributes(self):
         category_1 = {"category" : "Geography" }
         card_1 = Card("What is the capital of Alaska?", "Juneau", category_1)
+        category_2 = {"category": "Art"}
+        card_2 = Card("Who painted Starry Night?", "Vincent Van Gogh", category_2)
+        category_3 = {"category": "STEM"}
+        card_3 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars", category_3)
         turn_1 = Turn("Juneau", card_1)
+        turn_2 = Turn("Monet", card_2)
+        turn_3 = Turn("Mars", card_3)
         assert turn_1.guess == "Juneau"
+        assert turn_1.card == card_1
+        assert turn_2.guess == "Monet"
+        assert turn_2.card == card_2
+        assert turn_3.guess == "Mars"
+        assert turn_3.card == card_3
+
+    def test_it_can_tell_accuracy(self):
+        category_1 = {"category": "Geography"}
+        card_1 = Card("What is the capital of Alaska?", "Juneau", category_1)
+        category_2 = {"category": "Art"}
+        card_2 = Card("Who painted Starry Night?", "Vincent Van Gogh", category_2)
+        category_3 = {"category": "STEM"}
+        card_3 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars", category_3)
+        turn_1 = Turn("Juneau", card_1)
+        turn_2 = Turn("Monet", card_2)
+        turn_3 = Turn("Mars", card_3)
+        assert turn_1.correct() == True
+        assert turn_2.correct() == False
+        assert turn_3.correct() == True
+
+    def test_it_gives_feedback(self):
+        category_1 = {"category": "Geography"}
+        card_1 = Card("What is the capital of Alaska?", "Juneau", category_1)
+        category_2 = {"category": "Art"}
+        card_2 = Card("Who painted Starry Night?", "Vincent Van Gogh", category_2)
+        category_3 = {"category": "STEM"}
+        card_3 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars", category_3)
+        turn_1 = Turn("Juneau", card_1)
+        turn_2 = Turn("Monet", card_2)
+        turn_3 = Turn("Mars", card_3)
+        assert turn_1.feedback() == "Great job! Correct!"
+        assert turn_2.feedback() == "Not quite. Try again!"
+        assert turn_3.feedback() == "Great job! Correct!"
+
 
 if __name__ == '__main__':
     unittest.main()
